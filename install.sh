8972ef080f60783c58d835cc3254958b374479dd
Kevin Beaty <kevin@simplectic.com>
2011-01-16 13:44:51 -0600

    Add bash and ratpoison rc

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 # Install git submodules
 #git submodule update --init
 
-# Link dot vims
+#Link dot files
+#ln -s $PWD/bash_profile ~/.bash_profile
+#ln -s $PWD/bashrc ~/.bashrc
+#ln -s $PWD/xinitrc ~/.xinitrc
+#ln -s $PWD/inputrc ~/.inputrc
+#ln -s $PWD/ratpoisonrc ~/.ratpoisonrc
 #ln -s $PWD/vimrc ~/.vimrc
 #ln -s $PWD/vim ~/.vim
+
