20e69dc5263663e3753394a4a6125f557ad59897
Kevin Beaty <kevin@simplectic.com>
2011-12-19 23:23:36 -0600

    Add tmux.conf

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@
 #ln -s $PWD/bashrc ~/.bashrc
 #ln -s $PWD/xinitrc ~/.xinitrc
 #ln -s $PWD/inputrc ~/.inputrc
+#ln -s $PWD/tmux.conf ~/.tmux.conf
 #ln -s $PWD/ratpoisonrc ~/.ratpoisonrc
 #ln -s $PWD/vimrc ~/.vimrc
 #ln -s $PWD/vim ~/.vim
